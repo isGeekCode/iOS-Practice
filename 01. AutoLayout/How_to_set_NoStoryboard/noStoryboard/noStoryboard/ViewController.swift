b1ded3c417d068a725137bff3912dc2b0e4df0d8
isGeekCode <ptuguitar@naver.com>
2022-11-07 17:09:53 +0900

    [ADD] init

---
+++ 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  noStoryboard
+//
+//  Created by hyeonseok on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
