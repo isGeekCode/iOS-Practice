2034468f77dcbf68a4f0ebaf230b698aed168a86
isGeekCode <ptuguitar@naver.com>
2022-11-07 17:29:48 +0900

    [fix] bracket 누락

--- 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/SceneDelegate.swift
+++ 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/SceneDelegate.swift
@@ -22,7 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     window?.rootViewController = viewController
     window?.makeKeyAndVisible()
     window?.windowScene = windowScene
-
+  }
+  
   func sceneDidDisconnect(_ scene: UIScene) {
     // Called as the scene is being released by the system.
     // This occurs shortly after the scene enters the background, or when its session is discarded.
