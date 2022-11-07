91ed86f9387f6ffa569db98741a6d1240d17dc92
isGeekCode <ptuguitar@naver.com>
2022-11-07 17:30:13 +0900

    [ADD] previewView 세팅

--- 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/ViewController.swift
+++ 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/ViewController.swift
@@ -12,8 +12,30 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = .yellow
   }
-
-
 }
 
+#if DEBUG
+// step1. opt + cmd + enter: preview패널 열기
+// step2. resume 클릭
+// setp3. opt + p: preview 돌리기
+
+import SwiftUI
+
+struct ViewControllerPresentable: UIViewControllerRepresentable {
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+  }
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    ViewController()
+  }
+}
+
+struct ViewControllerPrepresentable_PreviewProvider: PreviewProvider {
+  static var previews: some View {
+    ViewControllerPresentable()
+  }
+}
+
+#endif
