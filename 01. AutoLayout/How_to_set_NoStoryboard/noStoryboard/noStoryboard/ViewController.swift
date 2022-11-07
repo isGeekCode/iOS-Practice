ba2ed5329c5e52a3e44e5d4654692cf2cea06b4c
isGeekCode <ptuguitar@naver.com>
2022-11-07 18:18:41 +0900

    [ADD] 코드로 오토레이아웃설정하기

--- 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/ViewController.swift
+++ 01. AutoLayout/How_to_set_NoStoryboard/noStoryboard/noStoryboard/ViewController.swift
@@ -12,7 +12,22 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = .yellow
+    self.view.backgroundColor = .white
+    
+    let someView = UIView()
+    self.view.addSubview(someView)
+    someView.backgroundColor = .yellow
+    
+    //시스템은 뷰의 프레임과 자동 크기 조정 마스크를 기반으로 한세트의 제약조건을 자동으로 생성한다
+    someView.translatesAutoresizingMaskIntoConstraints = false
+    
+    //크기
+    someView.widthAnchor.constraint(equalToConstant:200).isActive = true
+    someView.heightAnchor.constraint(equalToConstant:200).isActive = true
+    
+    //위치
+    someView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    someView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
   }
 }
 
