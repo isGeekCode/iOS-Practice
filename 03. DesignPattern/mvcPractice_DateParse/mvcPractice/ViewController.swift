fcebf467f142ab67570c8f0d583ad2b5a61fdecf
isGeekCode <ptuguitar@naver.com>
2023-02-08 07:50:28 +0900

    move mvc practice

---
+++ 03. DesignPattern/mvcPractice_DateParse/mvcPractice/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  mvcPractice
+//
+//  Created by bang_hyeonseok on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentDate = Date()
+
+    //MARK: - IBActions
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    
+    @IBAction func onYesterday(_ sender: Any) {
+    }
+    @IBAction func onNow(_ sender: Any) {
+    }
+    @IBAction func onTomorrow(_ sender: Any) {
+    }
+    
+    // MARK: - Life-cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
